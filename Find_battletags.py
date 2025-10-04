5debafbc3b0a63c0e03031a0d877a2656b41d985
RoryTidmarsh <151524572+RoryTidmarsh@users.noreply.github.com>
2025-10-04 20:04:50 +0100

    Restructure into functions

--- Find_battletags.py
+++ Find_battletags.py
@@ -19,7 +19,7 @@ base_url = "https://api.clashofclans.com/v1"
 url = base_url + f"/clans/{clan_tag}"
 headers = {
     "Accept": "application/json",
-    "authorization": "Bearer %s" % keys["rory_desktop"],
+    "authorization": "Bearer %s" % keys["rory_laptop"],
 }
 debug_print_statements = False
 # Make the request to the API
@@ -40,25 +40,54 @@ debug_print_statements = False
 # data_warlog = response_warlog.json()
 # print("War log keys: ", data_warlog["items"][0].keys()) # Keys: ['result', 'endTime', 'teamSize', 'attacksPerMember', 'battleModifier', 'clan', 'opponent']
 
+# Response codes from the API
+response_codes = {
+    200: "Success",
+    400: "Client provided incorrect parameters for the request.",
+    403: "Access denied, either because of missing/incorrect credentials or used API token does not grant access to the requested resource. Check the Api Key.",
+    404: "The requested resource does not exist.",
+    429: "Too many requests. You are rate limited. Wait before sending more requests.",
+    500: "Server error. Something is wrong on Clash of Clans side.",
+    503: "Server is down for maintenance.",
+}
 
-# Accessing CWL information
-leaguegroup_url = f"{base_url}/clans/{clan_tag}/currentwar/leaguegroup"
-leaguegroup_response = requests.get(leaguegroup_url, headers=headers)
-print("League group response status code:", leaguegroup_response.status_code)
-leaguegroup_data = leaguegroup_response.json()  # Keys: ['state', 'season', 'clans', 'rounds'])
-# print("League group keys: ", leaguegroup_data.keys())
-season = leaguegroup_data["season"]
-# print("League Other Clan members: ", leaguegroup_data["clans"][0].keys()) # Index for other clans in league group
-# print(leaguegroup_data)
+def get_war_tags(clan_tag, headers = headers):
+    """Get the war tags for the current war league of the clan.
 
-# Create a DataFrame to store battle tags in
-battle_tag_df = pd.DataFrame(columns=["battleday", "wartag1", "wartag2", "wartag3", "wartag4", "season"])
+    Args:
+        clan_tag (str): The clan tag of the clan to get the war league information for.
+        headers (dict): The headers to use for the API request.
+    Returns:
+        dict: The current war league information for the clan.
+    """
+    # Accessing CWL information
+    leaguegroup_url = f"{base_url}/clans/{clan_tag}/currentwar/leaguegroup"
+    leaguegroup_response = requests.get(leaguegroup_url, headers=headers)
+    print("League group response status code:", leaguegroup_response.status_code)
 
-# Tags each war in the league group
-cwl_war_tags = leaguegroup_data["rounds"]
-for i,battleday_tags in enumerate(cwl_war_tags):
-    battleday_tags = battleday_tags["warTags"]
-    battle_tag_df.loc[i] = [i+1, battleday_tags[0], battleday_tags[1], battleday_tags[2], battleday_tags[3], season]
+    # Check for errors in the response and print
+    if leaguegroup_response.status_code != 200:
+        print("Error: ", response_codes[leaguegroup_response.status_code])
+        exit()
+
+    # Get the JSON data from the response
+    leaguegroup_data = leaguegroup_response.json()  # Keys: ['state', 'season', 'clans', 'rounds'])
+    season = leaguegroup_data["season"]
+
+    # Create a DataFrame to store battle tags in
+    battle_tag_df = pd.DataFrame(columns=["battleday", "wartag1", "wartag2", "wartag3", "wartag4", "season"])
+
+    # Tags each war in the league group
+    cwl_war_tags = leaguegroup_data["rounds"]
+    for i,battleday_tags in enumerate(cwl_war_tags):
+        battleday_tags = battleday_tags["warTags"]
+        battle_tag_df.loc[i] = [i+1, battleday_tags[0], battleday_tags[1], battleday_tags[2], battleday_tags[3], season]
+
+    # Find how many rows empty tags there are "#0"
+    empty_tags = battle_tag_df["wartag1"].value_counts().get("#0", 0)
+    if empty_tags > 0:
+        print(f"Found {empty_tags} empty days in the war tag DataFrame.")
+    return battle_tag_df, season
 
 def append_days_to_dataframe(existing_data, new_df, season):
     """Append new battleday data to the existing DataFrame for the current season.
@@ -74,6 +103,7 @@ def append_days_to_dataframe(existing_data, new_df, season):
     # Check if the current season is already in the DataFrame
     if season in existing_data["season"].values:
         print(f"Season {season} is already in the DataFrame.")
+
         # Filter the existing data for the current season
         filt = existing_data["season"] == season
 
@@ -89,16 +119,6 @@ def append_days_to_dataframe(existing_data, new_df, season):
         newdata = pd.concat([existing_data, new_df], ignore_index=True)
     
     return newdata
-
-# Save the battle tag DataFrame to a CSV file
-save_filepath = os.path.join(os.path.dirname(__file__), "battle_tags.csv")
-existing_data = pd.read_csv(save_filepath)
-newdata = append_days_to_dataframe(existing_data,battle_tag_df, season)
-newdata.to_csv(save_filepath, index=False)
-
-# Print the DataFrame
-print("Battle tag DataFrame:")
-print(newdata)
 
 ############################################################################################
 ## Objective of the next part of the code:
@@ -135,35 +155,71 @@ def Pussay_in_war(battle_tag):
             print(f"Clan is not in war {battle_tag}")  
         return False
     
-# Find which wars the clan is in
-reduced_warTag_df = pd.DataFrame(columns=["battleday", "wartag", "season"])
-# Loop through the days
-for day, battle_day_tags in enumerate(cwl_war_tags):
-    battleday_tags = battle_day_tags["warTags"]
+def wars_with_clan(battle_tags):
+    """Check which wars in the list contain the clan.
 
-    # Loop through the battle tags for each day
-    for battle_tag in battleday_tags:
-        # Check if the clan is in the war
-        in_war = Pussay_in_war(battle_tag)
-        # If the clan is in the war, add the battle tag to the DataFrame
-        if in_war:
-            reduced_warTag_df.loc[len(reduced_warTag_df)] = [day+1, battle_tag, season]
-            if debug_print_statements:
-                print("Added to reduced DataFrame: ", battle_tag)
+    Args:
+        battle_tags (list): list of battle tags to check
 
-# Save the reduced battle tag DataFrame to a CSV file
-save_filepath = os.path.join(os.path.dirname(__file__), "Pussay_battle_tags.csv")
+    Returns:
+        dict: dictionary with the war day as the key and the battle tag as the value, or "#0" if the clan is not in the war
+        """
+    # 7 day dictionary of war tags containing "#0" for empty tags
+    clan_war_tags = {1: "#0", 2: "#0", 3: "#0", 4: "#0", 5: "#0", 6: "#0", 7: "#0"}
 
-# Check if the file already exists
-if os.path.exists(save_filepath):
-    existing_pussay_data = pd.read_csv(save_filepath) # If it exists, load the existing data and append the new data
-else:
-    existing_pussay_data = pd.DataFrame(columns=["battleday", "wartag", "season"])# Else, create a new DataFrame
+    # Loop through the rows of the DataFrame
+    for row in seasonal_battle_tag_df.itertuples():        
+        # Find which wartag has Pussay in the war for that day
+        for wartag in [row.wartag1, row.wartag2, row.wartag3, row.wartag4]:
+            if wartag != "#0": # Skip empty tags
+                in_war = Pussay_in_war(wartag)
+                if in_war:
+                    print(f"Pussay is in war {wartag} on day {row.battleday} of season {row.season}")
+                    clan_war_tags[row.battleday] = wartag
+                    break # Exit the loop if we found the clan in the war
+        # Create error messages
+        if clan_war_tags[row.battleday] == "#0":
+            print(f"Error: Clan not found in any war on day {row.battleday} of season {row.season}")
 
-# Append the new data to the existing data
-new_pussay_data = append_days_to_dataframe(existing_pussay_data, reduced_warTag_df, season)
-# Save the new data to the CSV file
-new_pussay_data.to_csv(save_filepath, index=False)
+    return clan_war_tags
 
-print("Reduced battle tag DataFrame:")
-print(new_pussay_data)
+if __name__ == "__main__":
+    # Get the battle tags for the current war league
+    seasonal_battle_tag_df, season = get_war_tags(clan_tag, headers)
+
+    # Save the battle tag DataFrame to a CSV file
+    save_filepath = os.path.join(os.path.dirname(__file__), "battle_tags.csv")
+    existing_data = pd.read_csv(save_filepath)
+    newdata = append_days_to_dataframe(existing_data, seasonal_battle_tag_df, season)
+    newdata.to_csv(save_filepath, index=False)
+
+    # Print the DataFrame
+    print("Battle tag DataFrame:")
+    print(newdata.tail(8))
+
+    # Find which wars the clan is in
+    clan_war_tags = wars_with_clan(seasonal_battle_tag_df)    
+    print("Clan war tags for the week:", clan_war_tags)
+    # Create a reduced DataFrame with only the war tags containing the clan, columns: battleday, wartag, season
+    reduced_warTag_df = pd.DataFrame(columns=["battleday", "wartag", "season"])
+    for day, tag in clan_war_tags.items():
+        reduced_warTag_df = pd.concat([reduced_warTag_df, pd.DataFrame({"battleday": [day], "wartag": [tag], "season": [season]})], ignore_index=True)
+    # print("Reduced war tag DataFrame:")
+    # print(reduced_warTag_df)
+
+    # Save the reduced battle tag DataFrame to a CSV file
+    save_filepath = os.path.join(os.path.dirname(__file__), "Pussay_battle_tags.csv")
+
+    # Check if the file already exists
+    if os.path.exists(save_filepath):
+        existing_pussay_data = pd.read_csv(save_filepath) # If it exists, load the existing data and append the new data
+    else:
+        existing_pussay_data = pd.DataFrame(columns=["battleday", "wartag", "season"])# Else, create a new DataFrame
+
+    # Append the new data to the existing data
+    new_pussay_data = append_days_to_dataframe(existing_pussay_data, reduced_warTag_df, season)
+    # Save the new data to the CSV file
+    new_pussay_data.to_csv(save_filepath, index=False)
+
+    print("Reduced battle tag DataFrame:")
+    print(new_pussay_data)
