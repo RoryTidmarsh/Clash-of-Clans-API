22c3621ab182f3587dcb57cc1aa874cf7079c872
Rory Tidmarsh <151524572+RoryTidmarsh@users.noreply.github.com>
2025-12-10 21:44:45 +0000

    Fix to refreshing data
    Supabaserefresh was not imported correctly causing data to not be correctly inserted

--- refresh/supabaseRefresh.py
+++ refresh/supabaseRefresh.py
@@ -5,7 +5,7 @@ from supabase import create_client
 import os
 from dotenv import load_dotenv
 
-load_dotenv(os.path.join(os.path.dirname(__file__), '.env.refresh'))
+load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
